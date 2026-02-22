c37ddea1be405c45f3a65ca4cbd6793dfeb55b7f
Thomas Knutsen <knutsenthomas@gmail.com>
2026-02-22 20:18:38 +0100

    CRITICAL DEPLOY: Resetting deploy branch to static stable files

--- dist/assets/firebase-config-BNsh33Gw.js
+++
@@ -1,1 +0,0 @@
-const i="AIzaSy",s="AelVsZnTU5xjQsjewWG7RjYEsQSHH-bkE";window.firebaseConfig={apiKey:i+s,authDomain:"his-kingdom-ministry.firebaseapp.com",projectId:"his-kingdom-ministry",storageBucket:"his-kingdom-ministry.appspot.com",messagingSenderId:"791237361706",appId:"1:791237361706:web:63516ba3d74436f23ac353",measurementId:"G-5CH82CHQ0B"};
