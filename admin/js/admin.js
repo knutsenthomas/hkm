176568fce2b08926b3e38f4c9183ff0cb14c2d9b
Thomas Knutsen <knutsenthomas@gmail.com>
2026-04-27 12:21:41 +0200

    fix: final syntax correction for hero slide upload

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -6294,6 +6294,8 @@ class AdminManager {
             } else {
                 imgTrigger.innerHTML = '<span class="material-symbols-outlined" style="opacity:0.3; font-size:48px;">add_a_photo</span>';
             }
+        };
+
         imgTrigger.onclick = () => fileInput.click();
 
         // File Upload (Blog-style Solution)
